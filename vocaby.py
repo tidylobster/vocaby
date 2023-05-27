d849c73b3cf12fa04b89987fc9e0c551a9f564a0
Ilnur Garifullin <ilnur.email@gmail.com>
2023-05-27 15:26:53 +0400

    Small improvements

--- vocaby.py
+++ vocaby.py
@@ -9,23 +9,28 @@ import textwrap
 from typing import List
 from sqlite3 import Connection
 from dataclasses import dataclass
+from tabulate import tabulate
 import openai
 
-openai.api_key = "sk-..."
+
+with open("config.json", "r") as file:
+    config = json.load(file)
+openai.api_key = config["token"]
+
 
 BASE_PATH = ".vocaby/"
 MODEL_ID = "gpt-3.5-turbo"
 PROMPT = """
 I need you to translate some words to russian for me. I will send you lines, 
-each continaing the word that needs to be translated and a sentence in which 
+each containing the word that needs to be translated and a sentence in which 
 it was used. The word and the sentence are separated by ::. Answer in JSON, 
 where you should include the original normalized word (name the field 
 "word"), translation of the word (name the field "translation"), transcription 
 of the word (name the field "transcription"), and the sentence (name the field 
-"usage") in which it was used where you should highlight the original word in 
-bold using html tag <b>. Do not translate the sentence. Only highlight one word 
-in the sentence, that was requested to be translated. Respond with a JSON 
-list, where you should include all of the translations.
+"usage") in which it was used. For the usage field you should highlight the 
+original word in bold using html tag <b>. Do not translate the sentence. Only 
+highlight the original word in the sentence, that was requested to be translated. 
+Respond with a JSON list, where you should include all of the translations.
 """
 
 
@@ -50,8 +55,9 @@ class Translation:
 @click.command()
 @click.option("--limit", default=20, help="Number of lookup words to scan")
 @click.option("--no-checkpoint", is_flag=True, default=False, help="Whether to update checkpoints")
+@click.option("--stats", is_flag=True, default=False, help="Show only stats")
 @click.argument("filename")
-def vocaby(limit, no_checkpoint, filename):
+def vocaby(limit, no_checkpoint, stats, filename):
     """
     Parse Kindle vocab.db file and transform the lookup words
     into Anki flashcards.
@@ -61,41 +67,53 @@ def vocaby(limit, no_checkpoint, filename):
 
     conn = sqlite3.connect(filename)
     num_unprocessed = get_unprocessed_words_count(conn, timestamp)
+    history = load_history()
+    by_book = get_unprocessed_words_count_by_books(conn, timestamp)
     if num_unprocessed == 0:
         return click.echo("No new words")
     click.echo(f"Words left unprocessed in the database: {num_unprocessed}")
+    click.echo(tabulate(by_book))
 
-    confirmed: List[Translation] = []
-    rejected: List[Translation] = []
-    for tr in query_gpt(query_db(conn, timestamp, limit)):
+    if not stats:
+        confirmed: List[Translation] = []
+        rejected: List[Translation] = []
+        for i, tr in enumerate(query_gpt(query_db(conn, timestamp, limit))):
+            click.clear()
+            click.echo(f"{i + 1}/{limit}\n")
+            click.echo(f"{tr.word} [{tr.transcription}] = {tr.translation}\n")
+            if tr.word in history:
+                click.echo("#"*80)
+                click.echo("This word was already processed!\n\n".upper())
+                click.echo("\n---\n".join(map(lambda x: textwrap.fill(x, width=80), history[tr.word])))
+                click.echo("#"*80 + "\n")
+            click.echo(textwrap.fill(tr.usage, width=80))
+            click.echo("")
+            click.echo(f"{tr.book}")
+            click.echo("---\n")
+            if click.confirm("Save this translation?"):
+                confirmed.append(tr)
+                history.setdefault(tr.word.lower(), []).append(tr.usage)
+            else:
+                rejected.append(tr)
         click.clear()
-        click.echo(f"{tr.word} [{tr.transcription}] = {tr.translation}\n")
-        click.echo(textwrap.fill(tr.usage, width=80))
-        click.echo("---\n")
-        click.echo(f"{tr.book}")
-        click.echo("---\n")
-        if click.confirm("Save this translation?"):
-            confirmed.append(tr)
-        else:
-            rejected.append(tr)
-    click.clear()
 
-    combined = list(itertools.chain(confirmed, rejected))
-    if len(combined) == 1:
-        timestamp = combined[0]
-    else:
-        timestamp = max(map(lambda x: x.timestamp, combined))
-    
-    filename = dump(confirmed, "anki")
-    click.echo(f"Saved new words to {filename}")
-    
-    if no_checkpoint:
-        click.echo("Skipped updating checkpoints")
-    else:
-        click.echo("Updated checkpoints")
-        save_latest_timestamp(timestamp)
-        num_unprocessed = get_unprocessed_words_count(conn, timestamp)
-        click.echo(f"Words left unprocessed in the database: {num_unprocessed}")
+        combined = list(itertools.chain(confirmed, rejected))
+        if len(combined) == 1:
+            timestamp = combined[0]
+        else:
+            timestamp = max(map(lambda x: x.timestamp, combined))
+        
+        filename = dump(confirmed, "anki")
+        click.echo(f"Saved new words to {filename}")
+        
+        if no_checkpoint:
+            click.echo("Skipped updating checkpoints")
+        else:
+            click.echo("Updated checkpoints")
+            save_latest_timestamp(timestamp)
+            save_history(history)
+            num_unprocessed = get_unprocessed_words_count(conn, timestamp)
+            click.echo(f"Words left unprocessed in the database: {num_unprocessed}")
     conn.close()
 
 
@@ -112,6 +130,19 @@ def save_latest_timestamp(timestamp: int):
         json.dump({"timestamp": timestamp}, file)
 
 
+def load_history():
+    if not os.path.exists(os.path.join(BASE_PATH, "history.json")):
+        return {}
+    with open(os.path.join(BASE_PATH, "history.json")) as file:
+        return json.load(file)
+
+
+def save_history(history):
+    os.makedirs(BASE_PATH, exist_ok=True)
+    with open(os.path.join(BASE_PATH, "history.json"), "w") as file:
+        json.dump(history, file, indent=2)
+    
+
 def get_unprocessed_words_count(conn: Connection, timestamp: int):
     cursor = conn.cursor()
     cursor.execute("""
@@ -121,8 +152,20 @@ def get_unprocessed_words_count(conn: Connection, timestamp: int):
     return cursor.fetchone()[0]
 
 
+def get_unprocessed_words_count_by_books(conn: Connection, timestamp: int):
+    cursor = conn.cursor()
+    cursor.execute("""
+        select b.title, count(*) from lookups l
+        join BOOK_INFO b on l.book_key = b.id
+        where l.timestamp > ? and l.word_key like 'en:%'
+        group by b.title 
+        order by count(*) desc
+    """, (timestamp,))
+    return cursor.fetchall()
+
+
 def query_db(conn: Connection, timestamp: int, limit: int) -> List[Lookup]:
-    click.echo("Retrieving words from database")
+    click.echo(f"Retrieving {limit} words from database")
     cursor = conn.cursor()
     cursor.execute("""
         select w.word, l.usage, b.title, l.timestamp from LOOKUPS l
@@ -136,7 +179,7 @@ def query_db(conn: Connection, timestamp: int, limit: int) -> List[Lookup]:
 
 
 def query_gpt(lookups: List[Lookup]) -> List[Translation]:
-    click.echo("Querying ChatGPT")
+    click.echo("Querying ChatGPT for translations")
     lines = []
     for word in lookups:
         lines.append(f"{word.word} :: {word.usage}")
@@ -166,7 +209,7 @@ def dump(words: List[Translation], prefix = ""):
     with open(filename, "w") as file:
         for word in words:
             word.usage
-            file.write(f"{word.word.lower()} [{word.transcription}] <hr>{word.usage}<br><br>{word.book}|{word.translation}\n")
+            file.write(f"{word.word.lower()} [{word.transcription}] <br><br>{word.usage}<br><br>{word.book}|{word.translation}\n")
     return filename
 
 
